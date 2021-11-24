4217e1e0bb8649852c5e73fa8b39a936c968863e
Asif9625 <asifrayhan9625@gmail.com>
2021-11-24 20:53:38 +0600

    3rd Commit

--- FINAL_LAB_PERFORMANCE_2/Calculator.php
+++ FINAL_LAB_PERFORMANCE_2/Calculator.php
@@ -1,5 +1,17 @@
-<<?php
-
-	echo "response: ".$_REQUEST[''];
-
+<?php
+ $i1 = $_POST['name'];
+ $i2 = $_POST['id'];
+ $op = $_POST['op'];
+ switch($op)
+ {
+   case "+":
+   $output = $i1 + $i2;
+   echo $output;
+   break;
+   case "-":
+   $output = $i1 - $i2;
+   echo $output;
+   break;
+   case '*':
+ }
 ?>
